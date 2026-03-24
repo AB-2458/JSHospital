8f492f5fd515f47ed866ef8ab41ae6f4ec9a3a72
AB-2458 <atharvab2458@gmail.com>
2026-03-24 17:30:08 +0530

    UX Changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@
  */
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
 import { Doctors } from './pages/Doctors';
@@ -14,9 +15,20 @@ import { Services } from './pages/Services';
 import { Contact } from './pages/Contact';
 import { Reviews } from './pages/Reviews';
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
